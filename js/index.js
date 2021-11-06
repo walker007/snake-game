a90eca3147205402c4c483a92e62f2a9cca55f9e
Alex Júnior <alexjr.anp@gmail.com>
2021-11-06 15:16:29 -0300

    Estrutura inicial do game

---
+++ js/index.js
@@ -0,0 +1,11 @@
+let canvas = document.getElementById('snake');
+let context = canvas.getContext('2d');
+let box = 32;
+
+function IniciaCanvas ()
+{
+    context.fillStyle = '#000';
+    context.fillRect(0,0,16*box, 16*box);
+}
+
+IniciaCanvas();
